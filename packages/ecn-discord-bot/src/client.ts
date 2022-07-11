68e88a98a965506d90bb2f55ce6c2f68892388e2
kent426 <kenthuang426@gmail.com>
2022-07-11 19:11:24 +0800

    poc of replying to reaction

--- packages/ecn-discord-bot/src/client.ts
+++ packages/ecn-discord-bot/src/client.ts
@@ -1,3 +1,10 @@
 import { Client, Intents } from "discord.js";
 // Create a new client instance
-export const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+export const client = new Client({
+  intents: [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+  ],
+  partials: ["MESSAGE", "CHANNEL", "REACTION"],
+});
--- packages/ecn-discord-bot/src/main.ts
+++ packages/ecn-discord-bot/src/main.ts
@@ -4,6 +4,7 @@ import "dotenv/config";
 
 import { DISCORD_TOKEN } from "./constants";
 import { client } from "./client";
+import { MessageReaction, TextChannel } from "discord.js";
 
 // When the client is ready, run this code (only once)
 client.once("ready", () => {
@@ -13,6 +14,48 @@ client.once("ready", () => {
 client.on("interactionCreate", async (interaction) => {
   await executeAll(interaction);
   return;
+});
+
+client.on("messageCreate", async (msg) => {
+  // console.log("ct: ", msg);
+});
+
+const IfSpecificChan = (inputMsg: MessageReaction): boolean => {
+  return (
+    inputMsg.message.channelId === "995978559556427857" &&
+    inputMsg.count == 1 &&
+    inputMsg.emoji.name === "verifiedMsg"
+  );
+};
+
+client.on("messageReactionAdd", async (reaction, user) => {
+  // When a reaction is received, check if the structure is partial
+  if (reaction.partial) {
+    // If the message this reaction belongs to was removed, the fetching might result in an API error which should be handled
+    try {
+      const fullMessage = await reaction.fetch();
+
+      console.log("part react");
+      if (IfSpecificChan(fullMessage)) {
+        console.log("gota");
+        (client.channels.cache.get("995978559556427857") as TextChannel).send(
+          `gota: ${reaction.message.content} from ${user.username}`
+        );
+      }
+    } catch (error) {
+      console.error("Something went wrong when fetching the message:", error);
+      // Return as `reaction.message.author` may be undefined/null
+      return;
+    }
+  } else {
+    console.log("full react: ");
+    if (IfSpecificChan(reaction)) {
+      console.log("gota");
+      (client.channels.cache.get("995978559556427857") as TextChannel).send(
+        `gota: ${reaction.message.content} from ${user.username}`
+      );
+    }
+  }
 });
 
 // Login to Discord with your client's token
