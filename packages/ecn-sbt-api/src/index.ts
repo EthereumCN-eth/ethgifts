c04f49a417bc6d97b9d9e02394aa065124ba2ed0
Stevewu1234 <plgawu@163.com>
2022-08-02 18:30:39 +0800

    update express call test

--- packages/ecn-sbt-api/src/index.ts
+++ packages/ecn-sbt-api/src/index.ts
@@ -1,8 +1,22 @@
 import { app } from "./server";
 import * as dotenv from "dotenv";
+import bodyParser from "body-parser";
+import { generateSignature } from "./generateSign/index";
 
 dotenv.config(); // Load the environment variables
 const PORT = process.env.PORT || 3010;
+
+app.use(bodyParser.json());
+
+app.post("/sign", async (req, res) => {
+  const { discordId, expressId } = req.body;
+  const status = await generateSignature(discordId, expressId);
+  res.send({
+    success: status.success,
+    error: status.error,
+    signatureRecord: status.signatureRecord,
+  });
+});
 
 app
   .listen(PORT, () =>
@@ -11,3 +25,7 @@ app
   .on("error", (e) => {
     console.log("Api error happened: ", e.message);
   });
+
+app.get("", (req, res) => {
+  res.send({ status: "ok" });
+});
