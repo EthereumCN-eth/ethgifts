92fca232458114c5f5bec4a324b42a89179daf86
kent426 <kenthuang426@gmail.com>
2022-11-10 18:57:10 +0800

    fix(styles): fixed bilibili video style css problem

--- packages/ecn-web-frontend/src/components/shared/InfoDetailView/VideoSection.tsx
+++ packages/ecn-web-frontend/src/components/shared/InfoDetailView/VideoSection.tsx
@@ -21,6 +21,21 @@ export const VideoSection = ({
           width: 39.4vw;
           height: ${(39.4 / 758) * 424}vw;
           position: relative;
+          margin-bottom: 1em;
+          padding-bottom: 56.25%;
+          height: 0;
+          overflow: hidden;
+          max-width: 100%;
+
+          iframe,
+          object,
+          embed {
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+          }
         `}
         // eslint-disable-next-line react/no-danger
         // dangerouslySetInnerHTML={{
