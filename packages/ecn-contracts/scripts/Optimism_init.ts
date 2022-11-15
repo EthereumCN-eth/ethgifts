76aa2ba530011a7d70507d5c34de9a5970205086
Stevewu1234 <plgawu@163.com>
2022-11-15 20:55:16 +0800

    add contract deployment interface

---
+++ packages/ecn-contracts/scripts/Optimism_init.ts
@@ -0,0 +1,62 @@
+import hre from 'hardhat';
+
+export const MergeParty_init = async (
+  mergeParty_address: string,
+  MergeNFT_merkleRoot: string,
+  MergeNFT_baseUri: string
+) => {
+  try {
+    if (MergeNFT_merkleRoot === '' || MergeNFT_baseUri === '') {
+      throw new Error('argus not set');
+    }
+
+    const mergePartyFactory = await hre.ethers.getContractFactory('MergeParty');
+    await mergePartyFactory
+      .attach(mergeParty_address)
+      .initializeEvent(MergeNFT_merkleRoot, MergeNFT_baseUri);
+    return {
+      success: true,
+      data: {
+        merkleRoot: MergeNFT_merkleRoot,
+        BaseUri: MergeNFT_baseUri,
+      },
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      success: false,
+      data: null,
+    };
+  }
+};
+
+export const Anniversary_init = async (
+  anni4_address: string,
+  Anni4_merkleRoot: string,
+  Anni4_baseUri: string
+) => {
+  try {
+    if (Anni4_merkleRoot === '' || Anni4_baseUri === '') {
+      throw new Error('argus not set');
+    }
+
+    const AnniFactory = await hre.ethers.getContractFactory('ECNAnniversary4');
+    await AnniFactory.attach(anni4_address).initializeEvent(
+      Anni4_merkleRoot,
+      Anni4_baseUri
+    );
+    return {
+      success: true,
+      data: {
+        merkleRoot: Anni4_merkleRoot,
+        BaseUri: Anni4_baseUri,
+      },
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      success: false,
+      data: null,
+    };
+  }
+};
--- packages/ecn-contracts/scripts/config.ts
+++ packages/ecn-contracts/scripts/config.ts
@@ -1,8 +1,27 @@
 import { BigNumber } from 'ethers';
 
 export const ExpressSBT_config = {
-  Approver: '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266',
-  sbt_levels: [BigNumber.from(2), BigNumber.from(4), BigNumber.from(6)],
+  goerli: {
+    Approver: '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266',
+    sbt_levels: [BigNumber.from(2), BigNumber.from(4), BigNumber.from(6)],
+  },
+  optimism: {
+    Approver: '',
+    sbt_levels: [],
+  },
+};
+
+export const ECNAnniversary_4_config = {
+  goerli: {
+    merkleRoot:
+      '0x3bc418e849fb00b3ba1c688b150aa5a4ef7492bc6106436f936e9eee4e57fc6e',
+    baseUri: 'https://example.com',
+  },
+  optimism: {
+    contract_address: process.env.ECNAnniversary_Contracct_Address || '',
+    merkleRoot: '',
+    baseUri: '',
+  },
 };
 
 export const MergeParty_config = {
@@ -12,6 +31,7 @@ export const MergeParty_config = {
     baseUri: 'https://example.com',
   },
   optimism: {
+    contract_address: process.env.MergePary_Contract_Address || '',
     merkleRoot:
       '0x6b7291ed04b90e6b96bc3fce1b7be5cd5bd07297d4be98a397d19e7796048474',
     baseUri: 'ipfs://QmP4nnhyxXa5aqEUPP2r8gE4PCn6oSxBpch7yNnu379FsV',
