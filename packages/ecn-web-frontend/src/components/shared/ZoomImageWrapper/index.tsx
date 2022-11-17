b63108a79d874fa9bdc1e213b8409e097d330c6d
kent426 <kenthuang426@gmail.com>
2022-11-17 11:10:57 +0800

    fix(components): image zoom fix

--- packages/ecn-web-frontend/src/components/shared/ZoomImageWrapper/index.tsx
+++ packages/ecn-web-frontend/src/components/shared/ZoomImageWrapper/index.tsx
@@ -2,5 +2,5 @@ import type { ReactNode } from "react";
 import Zoom from "react-medium-image-zoom";
 
 export const ZoomImageWrapper = ({ children }: { children: ReactNode }) => (
-  <Zoom zoomMargin={100}>{children}</Zoom>
+  <Zoom>{children}</Zoom>
 );
