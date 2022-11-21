29bdf2dda26e9ad3fe0289ea7ca50b6def0427a6
kent426 <kenthuang426@gmail.com>
2022-11-21 10:52:49 +0800

    fix(utils): web api jwt secret

--- packages/ecn-web-api/src/auth/constants.ts
+++ packages/ecn-web-api/src/auth/constants.ts
@@ -1,4 +1,5 @@
 export const jwtConstants = {
   secret:
-    'fasdifbweeaifjaewnefw32r32r3r3fdcaer4r4ty6u76u7uthbrfer3xfkvc32e23ohnejfnewfr32423',
+    // 'fasdifbweeaifjaewnefw32r32r3r3fdcaer4r4ty6u76u7uthbrfer3xfkvc32e23ohnejfnewfr32423',
+    process.env.JWT_SECRET,
 };
