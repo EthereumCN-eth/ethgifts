ffa81f9779840adee08644c78a7348c71895ec39
Kent Huang <kent426@users.noreply.github.com>
2022-12-08 17:12:36 +0800

    feat(utils): auto logout for jwt (#28)

--- packages/ecn-web-frontend/src/components/AddressOrNetworkChange.tsx
+++ packages/ecn-web-frontend/src/components/AddressOrNetworkChange.tsx
@@ -1,26 +1,24 @@
-import { useEffect, useRef } from "react";
-import { useAccount } from "wagmi";
-
 // import { actions as globalActions } from "../state/global";
-import { useAppDispatch } from "@/state/reduxHooks";
+import { useAutoLogoutExpiredJWT } from "@/state/global/hooks";
 
 const AddressOrNetworkChange = () => {
   // const { chain } = useNetwork();
-  const { address } = useAccount();
-  const dispatch = useAppDispatch();
-  const previousAddrRef = useRef<string | undefined>(address);
-  useEffect(() => {
-    if (address !== previousAddrRef.current) {
-      // dispatch(
-      //   globalActions.setAuth({
-      //     accessToken: null,
-      //     auth_status: "unauthenticated",
-      //   })
-      // );
-    }
+  // const { address } = useAccount();
+  // const dispatch = useAppDispatch();
+  // const previousAddrRef = useRef<string | undefined>(address);
+  useAutoLogoutExpiredJWT();
+  // useEffect(() => {
+  //   if (address !== previousAddrRef.current) {
+  //     // dispatch(
+  //     //   globalActions.setAuth({
+  //     //     accessToken: null,
+  //     //     auth_status: "unauthenticated",
+  //     //   })
+  //     // );
+  //   }
 
-    previousAddrRef.current = address;
-  }, [address, dispatch]);
+  //   previousAddrRef.current = address;
+  // }, [address, dispatch]);
   return null;
 };
 
