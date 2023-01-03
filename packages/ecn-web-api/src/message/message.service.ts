448b4ad455db7f86b790d7d518fb29d4fccdf3c3
kent426 <kenthuang426@gmail.com>
2023-01-03 16:37:49 +0800

    feat(web-api): wip message api

--- packages/ecn-web-api/src/message/message.service.ts
+++ packages/ecn-web-api/src/message/message.service.ts
@@ -1,11 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from './../prisma/prisma.service';
 import { PrismaClient, Prisma, User } from '@prisma/client';
-
+import {
+  startOfMonth,
+  format,
+  endOfMonth,
+  min,
+  max,
+  add,
+  isBefore,
+  sub,
+  isAfter,
+} from 'date-fns';
 @Injectable()
 export class MessageService {
   constructor(private prisma: PrismaService) {}
-  async findLatestByDate() {
+  async findMessagesByDate({ datestring }: { datestring: string }) {
     const minmaxDate = await this.prisma.expressMessage.aggregate({
       _max: {
         verifiedAt: true,
@@ -13,12 +23,65 @@ export class MessageService {
       _min: {
         verifiedAt: true,
       },
-      _count: {
-        id: true,
+      // _count: {
+      //   id: true,
+      // },
+    });
+    const { _max, _min } = minmaxDate;
+    const currentDate = _max?.verifiedAt
+      ? max([min([_max.verifiedAt, new Date(datestring)]), _min.verifiedAt])
+      : new Date(datestring);
+
+    const messagesOfMonth = await this.prisma.expressMessage.findMany({
+      where: {
+        verifiedAt: {
+          lte: endOfMonth(currentDate),
+          gte: startOfMonth(currentDate),
+        },
+      },
+      orderBy: {
+        verifiedAt: 'asc',
       },
     });
 
-    return minmaxDate;
+    const maxDate = _max?.verifiedAt;
+    const minDate = _min?.verifiedAt;
+    const nextMonth = add(currentDate, {
+      months: 1,
+    });
+    // );
+    const previousMonth = sub(currentDate, {
+      months: 1,
+    });
+    // );
+    const nextMonthDate = isBefore(nextMonth, maxDate) ? nextMonth : null;
+    const previousMonthDate = isAfter(previousMonth, minDate)
+      ? previousMonth
+      : null;
+    return {
+      navInfo: {
+        maxDate,
+        minDate,
+        currentDate,
+        nextMonthDate,
+        previousMonthDate,
+      },
+
+      messagesOfMonth,
+    };
+    // const minmaxDate = await this.prisma.expressMessage.aggregate({
+    //   _max: {
+    //     verifiedAt: true,
+    //   },
+    //   _min: {
+    //     verifiedAt: true,
+    //   },
+    //   _count: {
+    //     id: true,
+    //   },
+    // });
+
+    // return minmaxDate;
   }
 
   findOne(id: number) {
