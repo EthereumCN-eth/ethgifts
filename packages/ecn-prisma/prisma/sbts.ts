509ce3a1804384e42483c2a33db0bc4157ff76a6
kent426 <kenthuang426@gmail.com>
2023-02-16 15:43:57 +0800

    added mock data(translator sbt)

--- packages/ecn-prisma/prisma/sbts.ts
+++ packages/ecn-prisma/prisma/sbts.ts
@@ -31,4 +31,55 @@ export const sbts: SBTCreateData[] = [
       },
     },
   },
+
+  {
+    contractAddress: "0xbBE11F912599ecb054145aAD81674c137bb42657",
+    contractName: "TranslatorSBT",
+    version: "1",
+    chainId: 5,
+    // token id here
+    countLevel: [3, 6, 20],
+    metaDataName: "TranslatorSBT",
+    metaDataDescription: "just a goerli test sbt",
+    issuerAddress: "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",
+    galleryItemBase: {
+      create: {
+        coverLink:
+          "https://ethereumcn.mypinata.cloud/ipfs/QmddhyXqz1A2jhCWFT5fwrHx4H57bYNp5eGYY6o2Vq7TQL",
+        chainId: 5,
+        name: "Translator SBT 系列",
+        onShelf: true,
+        galleryItemType: "sbt",
+        itemText: [
+          "Mainnet",
+          "Layer2",
+          "Staking",
+          "MEV",
+          "Cryptography",
+          "Tool&Tech",
+          "DeFi",
+          "NFT",
+          "SocialFi",
+          "Ecosystem",
+        ],
+        imageLinks: [
+          // "https://mirror-media.imgix.net/publication-images/cDosw4qHdNtOPrVxYn5CK.jpg?h=540&w=540",
+          "https://foundation.app/images/drops/collection-01-02@2x.png",
+          "https://foundation.app/images/drops/collection-03-03@2x.png",
+
+          "https://foundation.app/images/drops/collection-01-02@2x.png",
+          "https://foundation.app/images/drops/collection-03-03@2x.png",
+          "https://foundation.app/images/drops/collection-01-02@2x.png",
+          "https://foundation.app/images/drops/collection-03-03@2x.png",
+          "https://foundation.app/images/drops/collection-01-02@2x.png",
+          "https://foundation.app/images/drops/collection-03-03@2x.png",
+          "https://foundation.app/images/drops/collection-01-02@2x.png",
+          "https://foundation.app/images/drops/collection-03-03@2x.png",
+        ],
+        tags: ["SBT", "工作量证明"],
+        tokenType: "ERC1155",
+        eventStartTime: 1666076540,
+      },
+    },
+  },
 ];
